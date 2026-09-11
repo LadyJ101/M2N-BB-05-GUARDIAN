bc8b2b8c0689aa0bd919345a6bd40bea6303fa3f
samuel <taiwosamuel8364@gmail.com>
2026-09-11 23:28:23 +0100

    Refactor testbench for guardian_fsm: Enhance input/output monitoring and add validation checks
    - Updated the testbench to include active_out and fault_out signals.
    - Added validation checks after reset, activation, stopping, and fault conditions to ensure correct state transitions.
    - Improved display messages for better debugging and clarity during simulation.
    - Ensured proper handling of reset conditions and state assertions.

--- tb/tb_guardian_fsm.v
+++ tb/tb_guardian_fsm.v
@@ -6,6 +6,8 @@ module tb_guardian_fsm;
   reg fault_in;
   reg activate_in;
   reg stop_in;
+  wire active_out;
+  wire fault_out;
 
   wire [1:0] state;
 
@@ -16,12 +18,12 @@ module tb_guardian_fsm;
     .fault_in(fault_in),
     .activate_in(activate_in),
     .state(state),
-    .stop_in(stop_in)
+    .stop_in(stop_in),
+    .active_out(active_out),
+    .fault_out(fault_out)
   );
 
-    // Testbench logic can be added here to drive inputs and monitor outputs
 
-    // Example testbench logic
     initial begin
       $dumpfile("sim/guardian_fsm.vcd");
       $dumpvars(0, tb_guardian_fsm);
@@ -32,32 +34,95 @@ module tb_guardian_fsm;
       activate_in = 0;
       stop_in     = 0;
 
-      rst         = 1;
-      #10 $display("State after clk asserted: %b", state);
 
-      #10 rst = 1; // move to RESET
-      #10 $display("State after rst = 1 asserted: %b", state);
+        // move to RESET
+      rst = 1; 
+      #10 if (state == 2'b00 && active_out == 0 && fault_out == 0) begin
+        #10 $display("State after rst = 1 asserted: %b", state);
+        #10 $display("The value of active_out: %b", active_out);
+        #10 $display("The value of fault_out: %b", fault_out);        
+      end
+      else begin
+        #10 $display("XXXXX Test for rst = 1 failed");
+        #10 $display("State after rst = 1 asserted: %b", state);
+        #10 $display("The value of active_out: %b", active_out);
+        #10 $display("The value of fault_out: %b", fault_out); 
+        #10 $finish;         
+      end
 
-       // Apply reset
-      #10 rst = 0; 
-      #10 $display("State after rst = 0 asserted: %b", state); // Move to IDLE
 
-      #10 activate_in = 0;
-      #10 $display("State after activate_in = 0 asserted: %b", state);
+       // Start the system
+      #10 rst = 0;
+      #10 if (state == 2'b01 && active_out == 0 && fault_out == 0) begin
+        #10 $display("State after rst = 0 asserted: %b", state);
+        #10 $display("The value of active_out: %b", active_out);
+        #10 $display("The value of fault_out: %b", fault_out);        
+      end
+      else begin
+        #10 $display("XXXXX Test for rst = 0 failed");
+        #10 $display("State after rst = 0 asserted: %b", state);
+        #10 $display("The value of active_out: %b", active_out);
+        #10 $display("The value of fault_out: %b", fault_out); 
+        #10 $finish;         
+      end 
 
       #10 activate_in = 1;
-      #10 $display("State after activate_in = 1 asserted: %b", state); // Move to ACTIVE
+      #10 if (state == 2'b10 && active_out == 1 && fault_out == 0) begin
+        #10 $display("State after activate_in = 1 asserted: %b", state); // Move to ACTIVE
+        #10 $display("The value of active_out: %b", active_out);
+        #10 $display("The value of fault_out: %b", fault_out);        
+      end
+      else begin
+        #10 $display("XXXXX Test for activate_in = 1 failed");
+        #10 $display("State after actvate_in = 1 asserted: %b", state);
+        #10 $display("The value of active_out: %b", active_out);
+        #10 $display("The value of fault_out: %b", fault_out); 
+        #10 $finish;         
+      end
 
       #10 stop_in = 1;
-      #10 $display("State after stop_in = 1 asserted: %b", state); // Move to IDLE
+      #10 if (state == 2'b01 && active_out == 0 && fault_out == 0) begin
+        #10 $display("State after stop_in = 1 asserted: %b", state); // Move to IDLE
+        #10 $display("The value of active_out: %b", active_out);
+        #10 $display("The value of fault_out: %b", fault_out);        
+      end
+      else begin
+        #10 $display("XXXXX Test for stop_in = 1 failed");
+        #10 $display("State after stop_in = 1 asserted: %b", state); // Move to IDLE
+        #10 $display("The value of active_out: %b", active_out);
+        #10 $display("The value of fault_out: %b", fault_out); 
+        #10 $finish;         
+      end
       
-      // Apply test stimuli
+      // Apply fault, system move to protection mode immediately
       #10 fault_in = 1;
-      #10 $display("State after fault_in = 1  asserted: %b", state);
+      #10 if (state == 2'b11 && active_out == 0 && fault_out == 1) begin
+        #10 $display("State after fault_in = 1  asserted: %b", state);
+        #10 $display("The value of active_out: %b", active_out);
+        #10 $display("The value of fault_out: %b", fault_out);        
+      end
+      else begin
+        #10 $display("XXXXX Test for fault_in = 1 failed");
+        #10 $display("State after fault_in = 1  asserted: %b", state);
+        #10 $display("The value of active_out: %b", active_out);
+        #10 $display("The value of fault_out: %b", fault_out); 
+        #10 $finish;         
+      end
      
-      #10 rst = 1; 
-      #10 $display("State after rst = 1 asserted: %b", state); // Move to RESET
-
+      // Reset the system
+      #10 rst = 1;
+      #10 if (state == 2'b00 && active_out == 0 && fault_out == 0) begin
+        #10 $display("State after rst = 1 asserted: %b", state); // Move to RESET
+        #10 $display("The value of active_out: %b", active_out);
+        #10 $display("The value of fault_out: %b", fault_out);        
+      end
+      else begin
+        #10 $display("XXXXX Test for rst_in = 1 failed");
+        #10 $display("State after rst = 1 asserted: %b", state); // Move to RESET
+        #10 $display("The value of active_out: %b", active_out);
+        #10 $display("The value of fault_out: %b", fault_out); 
+        #10 $finish;         
+      end
 
       #10 $display("Final state: %b", state);
       // End simulation
